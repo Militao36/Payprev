ebb4ba37f9e416144cb6e07540ca19664db98ad6
Militao36 <matheusmoreira3693@gmail.com>
2019-08-31 19:10:28 -0300

    Commit

--- dist/config/Secret.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secret: 'Teste_dev_back-end_Payprev',
-};
-//# sourceMappingURL=Secret.js.map
--- dist/enum/TipoUsuario.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var TipoUsuario;
-(function (TipoUsuario) {
-    TipoUsuario["ADMIN"] = "ADMIN";
-    TipoUsuario["COMUM"] = "COMUM";
-})(TipoUsuario = exports.TipoUsuario || (exports.TipoUsuario = {}));
-//# sourceMappingURL=TipoUsuario.js.map
