4a41e08d70a89134f0b0100402d6555f86cfdf6f
Militao36 <matheusmoreira3693@gmail.com>
2019-09-02 09:16:51 -0300

    Coomit

--- src/config/ConfigDb.ts
+++ src/config/ConfigDb.ts
@@ -3,9 +3,9 @@ import knex from 'knex';
 export default knex({
     client: 'mysql',
     connection: {
-        host: 'localhost',
-        user: 'root',
-        password: 'senhaBMS16!',
+        host: 'mysql669.umbler.com',
+        user: 'payprev',
+        password: 'payprev2019',
         database: 'payprev',
     },
 });
